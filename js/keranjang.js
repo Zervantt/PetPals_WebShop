43c279abde073e3bc5b8179a5954af6bd0b98d09
Hilman Arif <147929301+Zervantt@users.noreply.github.com>
2025-04-24 21:26:24 +0700

    Delete js/keranjang.js

--- js/keranjang.js
+++
@@ -1,7 +0,0 @@
-function updateQuantity(productId) {
-    var quantityInput = document.querySelector('#update-form-' + productId + ' input[name="quantity"]');
-    if (quantityInput.value > 20) {
-        quantityInput.value = 20;
-    }
-    document.getElementById('update-form-' + productId).submit();
-}
